1dd72b16e4cc8f67a5779f578ed6cd10dd452e7e
T2bex <tobechukwu14@yahoo.com>
2025-04-19 00:50:35 +0100

    Theme creation

--- flutter_rpg/lib/screens/home/home.dart
+++ flutter_rpg/lib/screens/home/home.dart
@@ -20,6 +20,7 @@ class _HomeState extends State<Home> {
          child: Column(
           children: [
             const Text('Character List'),
+            Text('Character list' ,style: Theme.of(context).textTheme.headlineMedium,),
 
             FilledButton(
               onPressed: () {},
